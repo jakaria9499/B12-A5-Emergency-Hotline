88ea1a5ecb4081e1e7d174b4addcd44db7493c0e
jakaria <jakaria9499@gmail.com>
2025-08-29 00:02:03 +0600

    calling alert and copy functionality included

--- script.js
+++ script.js
@@ -4,4 +4,29 @@ function heartIncrement() {
     document.getElementById("heart_display").innerText = ++currentDigit;
 }
 
+// calling alert
+let coin = 100;
+document.getElementById("coin").innerText = coin;
+let callBtnCollection = document.getElementsByClassName("call_btn");
+for(let i of callBtnCollection) {
+    i.addEventListener("click", function() {
+        if(coin>0) {
+            alert("calling " + i.parentElement.parentElement.childNodes[3].childNodes[1].innerText +" " + i.parentElement.parentElement.childNodes[5].childNodes[1].innerText );
+            coin-=20;
+            document.getElementById("coin").innerText = coin;
+        }
+        else{
+            alert("insufficient coins , you need to at least 20 coins to make a call");
+        }
+    })
+}
 
+// copy functionality
+let copyBtnCollection = document.getElementsByClassName("copy_btn");
+
+for(let i of copyBtnCollection) {
+    i.addEventListener("click", function() {
+        const copyText = i.parentElement.parentElement.childNodes[5].childNodes[1].textContent;
+        navigator.clipboard.writeText(copyText);
+    })
+}
