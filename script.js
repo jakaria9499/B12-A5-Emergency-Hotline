7db5fd59ab4ca9137441969d53ac5e3343bd8f25
jakaria <jakaria9499@gmail.com>
2025-08-28 21:15:47 +0600

    heart icon functionality added

---
+++ script.js
@@ -0,0 +1,7 @@
+// heart increment number function 
+function heartIncrement() {
+    let currentDigit = parseInt(document.getElementById("heart_display").innerText);
+    document.getElementById("heart_display").innerText = ++currentDigit;
+}
+
+
